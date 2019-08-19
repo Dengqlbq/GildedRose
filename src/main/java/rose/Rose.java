f2fe97d4d415f7fcaad99892a1454b56b8e2ff71
Dengqlbq <610426459@qq.com>
2019-08-19 22:27:40 +0800

    refactor: refactoring preHandleItem

--- src/main/java/rose/Rose.java
+++ src/main/java/rose/Rose.java
@@ -30,26 +30,27 @@ public class Rose {
     private void preHandleItem(Item item) {
         if (!item.name.equals("Aged Brie")
                 && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
-                }
+
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.quality -= item.quality > 0 ? 1 : 0;
             }
         } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
+            if (item.quality >= 50) {
+                return;
+            }
 
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
+            item.quality = item.quality + 1;
+
+            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
+                }
 
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
