8ac5302a06ab26e69c4fb8436d0afd1f21151d7b
Dengqlbq <610426459@qq.com>
2019-08-19 22:20:00 +0800

    refactor: refactoring handleItemWithSellInSmallerThanZero

--- src/main/java/rose/Rose.java
+++ src/main/java/rose/Rose.java
@@ -53,20 +53,20 @@ public class Rose {
     }
 
     private void handleItemWithSellInSmallerThanZero(Item item) {
-        if (!item.name.equals("Aged Brie")) {
-            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                item.quality = item.quality - item.quality;
-            }
-        } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-            }
+        if (item.name.equals("Aged Brie")) {
+            item.quality += item.quality < 50 ? 1 : 0;
+            return;
         }
+
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            item.quality = 0;
+            return;
+        }
+
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.quality -= item.quality > 0 ? 1 : 0;
+        }
+
+
     }
 }
